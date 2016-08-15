25296ef65e05607a05c6e520440d0008c3e92251
Jack Zhang <15jzhang@gmail.com>
2016-08-15 17:31:13 -0400

    Add Tweet example component

---
+++ examples/Tweet.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import createPropType from './../lib/create-proptype';
+
+const Tweet = props => (
+  <div>{props.text}</div>
+);
+
+Tweet.propTypes = {
+  text: createPropType(
+    prop => typeof prop === 'string' && prop.length < 140
+  ).isRequired
+};
+
+export default Tweet;

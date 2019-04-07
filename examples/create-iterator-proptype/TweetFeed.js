772096a6a37d052622222e55a0dcb1a9df647e22
Jack Zhang <15jzhang@gmail.com>
2019-04-07 01:25:44 -0400

    Import 'PropType' from 'prop-types' package
    This is necessary with upgrading to React 16.

--- examples/create-iterator-proptype/TweetFeed.js
+++ examples/create-iterator-proptype/TweetFeed.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import { PropTypes } from 'prop-types';
 import { createIteratorPropType } from './../../lib/custom-proptypes';
 
 const TweetFeed = props => (
